43d404b8a88672df3930eb715ff81b6c9e90ea98
Gaurav Kaushik <gaurav.kaushik@valamis.com>
2020-11-30 15:18:44 +0200

    adding Interfaces for services. Closes #5

---
+++ kaushik-pet-clinic-data/src/main/java/kaushik/springframework/kaushikpetclinic/services/PetService.java
@@ -0,0 +1,12 @@
+package kaushik.springframework.kaushikpetclinic.services;
+
+import kaushik.springframework.kaushikpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ kaushik-pet-clinic-data/src/main/java/kaushik/springframework/kaushikpetclinic/services/VetService.java
@@ -0,0 +1,12 @@
+package kaushik.springframework.kaushikpetclinic.services;
+
+import kaushik.springframework.kaushikpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
