6e8c8524eeb5779095166a4d0d1ebb74cbc9fe64
gauravkaushikcode <gori.kaushik@gmail.com>
2020-12-01 16:43:16 +0200

    adding vet and owner index page and controller. Closes #15. Closes #16

--- kaushik-pet-clinic-data/src/main/java/kaushik/springframework/kaushikpetclinic/bootstrap/DataLoader.java
+++ kaushik-pet-clinic-data/src/main/java/kaushik/springframework/kaushikpetclinic/bootstrap/DataLoader.java
@@ -54,6 +54,7 @@ public class DataLoader implements CommandLineRunner {
         vet2.setLastName("Porter");
 
         vetService.save(vet2);
+
         System.out.println("exiting data loader");
     }
 }
